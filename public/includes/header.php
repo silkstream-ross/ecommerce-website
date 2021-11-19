b06ac17f1e4f4fd8475a68ee1d07970de415fee9
Ross Wallings <ross@silkstream.net>
2021-11-19 15:35:37 +0000

    styled checkout button, increased text size of basket table and the header

--- public/includes/header.php
+++ public/includes/header.php
@@ -6,7 +6,7 @@
             <a id="menu_button" href="#"><img src="images/menu.svg" height="50" width="50" alt="Menu"></a>
         </div>
 
-
+    <div class="navbar">
         <ul>
             <li><a href="/basket.php"><img src="images/Basket.svg" alt="Basket"></a></li>
             <li><a href="#"><img src="images/Search.svg" alt="Search"></a></li>
@@ -15,10 +15,10 @@
             <li><a href="#">Category 2 </a></li>
             <li><a href="/home.php">Category 1 </a></li>
         </ul>
-
+    </div>
 
         <ul id="menu" style="z-index: 4">
-            <a href="js/main.js"></a>
+            <script href="js/main.js"></script>
             <div class="menu_content">
                 <a href="/home.php">Category 1</a>
                 <a href="#">Category 2</a>
